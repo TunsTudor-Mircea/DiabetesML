51a7e4b231fc62a01186370e2c51aeeee447eefc
Tudor Tuns <tudor.mircea.tuns@gmail.com>
2025-03-15 20:23:48 +0200

    Model Scrap

---
+++ src/main/java/model/DecisionTree.java
@@ -0,0 +1,17 @@
+package model;
+
+import data.Instance;
+
+import java.util.List;
+
+public class DecisionTree implements Model<Number, Integer> {
+    @Override
+    public void train(List<Instance<Number, Integer>> instances) {
+
+    }
+
+    @Override
+    public List<Integer> test(List<Instance<Number, Integer>> instances) {
+        return List.of();
+    }
+}
